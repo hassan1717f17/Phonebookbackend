9c9053579afe8517822ccd8b7f27a9d42f4187e5
hassan1717f17 <ihtisham1717f17@gmail.com>
2024-10-04 02:22:03 +0500

    MongoDB Integrated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app=express()
 
@@ -14,71 +15,55 @@ app.use(cors())
 
 app.use(express.static('dist'))
 
-let persons=[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons',(request, response)=>{
-    response.json(persons)
+    Person.find({})
+        .then(persons=>{
+            response.json(persons)
+        })
 })
 
 app.get('/api/persons/:id',(request,response)=>{
-    const id=request.params.id
-    const person=persons.find(person=>person.id===id)
-    person?response.json(person): response.status(404).end()
+    Person.findbyId(request.params.id)
+        .then(person=>{
+            if(person){
+                response.json(person)
+            }else{
+                response.status(404).end()
+            }
+        }).catch(error=>response.status(400).send({error:'malformatted id'}))
 })
 
 app.delete('/api/persons/:id',(request,response)=>{
-    const id=request.params.id
-    persons=persons.filter(person=>person.id!==id)
-
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(()=>response.status(204).end())
+        .catch(error=>response.status(400).send({error:'malformatted id'}))
 
 })
 
 app.post('/api/persons',(request,response)=>{
-    const person=request.body
+    const body = request.body
 
-    if(!person.name || !person.number){
+    if(!body.name || !body.number){
         return response.status(400).json({
             error:"name or number is missing"
-        }).end()
-    }
-
-    const existing = persons.find(p=>p.name===person.name)
-    if(existing){
-        return response.status(400).json({
-            error:"name must be unique"
         })
     }
-    person.id=String(Math.floor(Math.random()*(10000000-0+1)+0))
-    persons=persons.concat(person)
-    response.json(person)
+
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson=>{
+        response.json(savedPerson)
+    })
 })
 
 const date = new Date()
 
 app.get('/info',(request,response)=>{
-    let count= persons.length
+    Person.countDocument({}).then(count=>{
     response.send(`<p>Phonebook has info for ${count} people
         </br> 
         ${date.toLocaleString('en-US', { weekday: 'short' })}
@@ -87,7 +72,7 @@ app.get('/info',(request,response)=>{
         ${date.getFullYear()}
         ${date.toLocaleTimeString()}
         ${date.toLocaleTimeString('en-US', { timeZoneName: 'short' }).split(' ')[2]}</p>`)
-    
+    })
 })
 
 const PORT = process.env.PORT || 3001
